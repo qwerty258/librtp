19007d8083d42d325e86a0ea87c07d2923f8b09e
qwerty258 <zyf26256@gmail.com>
2015-11-16 10:55:30 +0800

    rename

--- src/libRTPSessions.h
+++ src/libRTPSessions.h
@@ -14,7 +14,7 @@ typedef SOCKET RTP_socket;
 #else
 #endif // _WIN32
 
-typedef struct _RTP_session
+typedef struct _RTP_session_context
 {
     RTP_socket sock;
     uint32_t local_IPv4;
@@ -22,6 +22,6 @@ typedef struct _RTP_session
     uint16_t local_port;
     uint16_t remote_port;
     uint32_t IP_protocol;
-}RTP_session;
+}RTP_session_context;
 
 #endif // !_LIBRTP_SESSIONS_H_
