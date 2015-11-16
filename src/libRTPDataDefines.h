4ce910d0f785e3fa29dc5880d569046f8695cf04
qwerty258 <zyf26256@gmail.com>
2015-11-16 18:10:54 +0800

    add data defines

---
+++ src/libRTPDataDefines.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _LIBRTP_DATA_DEFINES_H_
+#define _LIBRTP_DATA_DEFINES_H_
+
+#include <stdint.h>
+
+typedef struct _raw_socket_data
+{
+    uint8_t buffer[2000];
+    uint32_t size;
+}raw_socket_data;
+
+#endif // !_LIBRTP_DATA_DEFINES_H_
