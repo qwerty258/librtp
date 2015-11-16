93031fe290d838d42780eb157ef5d06c8ac301a8
qwerty258 <zyf26256@gmail.com>
2015-11-16 10:36:52 +0800

    add session struct

--- src/libRTPSessions.h
+++ src/libRTPSessions.h
@@ -6,11 +6,21 @@
 #else
 #endif // _WIN32
 
+#include <stdint.h>
+
 #ifdef _WIN32
 typedef SOCKET RTP_socket;
 #else
 #endif // _WIN32
 
-
+typedef struct _RTP_session
+{
+    RTP_socket sock;
+    uint32_t local_IPv4;
+    uint32_t remote_IPv4;
+    uint16_t local_port;
+    uint16_t remote_port;
+    uint32_t IP_protocol;
+}RTP_session;
 
 #endif // !_LIBRTP_SESSIONS_H_
