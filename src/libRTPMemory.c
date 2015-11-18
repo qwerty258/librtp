99c57911b48044a893e4e5639b472f38ee2ff8cd
qwerty258 <zyf26256@gmail.com>
2015-11-18 14:48:39 +0800

    add memory leak check

--- src/libRTPMemory.c
+++ src/libRTPMemory.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _MSC_VER
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // _DEBUG
+#endif // _MSC_VER
+
 void* libRTP_malloc(size_t size)
 {
     void* pointer = malloc(size);
